e541ae6aaea51a8e955dc3153303595945e9beb4
Jade Stewart <jadekstewart3@gmail.com>
2025-05-13 14:17:08 -0600

    Flesh out else if statements and terminal prints

--- DreamJournal.java
+++ DreamJournal.java
@@ -5,17 +5,34 @@ import java.time.LocalDate;
 public class DreamJournal {
   public static void  main(String[] args) {
     Scanner scanner = new Scanner(System.in); //read user input from keyboard
-    File dreamFile =new File("dreams.txt"); //create a file object
+    File dreamFile = new File("dreams.txt"); //create a file object
     boolean running = true; //flag to control the loop
 
     while (running) {
+      //menu display
       System.out.println("Welcome to your Dream Journal!");
       System.out.println("1. Write a new dream entry");
       System.out.println("2.Veiw all dreams in the clouds");
       System.out.println("3. EXIT");
       System.out.print("Enter your choice (1-3): ");
 
-      String choice = scanner.nextline(); //read user input
+      String choice = scanner.nextLine(); //read user input
+
+      if (menuChoice.equals("1")) {
+        //handle writing a new dream entry
+        System.out.println("Tell me your wildest dreams!");
+      } else if (menuChoice.equals("2")) {
+        //handle reading dreams from dreams.txt
+        System.out.println("Here are all your dreams in the clouds:");
+
+      } else if (menuChoice.equals("3")) {
+        //handle exiting the program
+        System.out.println("Goodbye!");
+        running = false; //sets flag to exit the loop
+      } else {
+        //handle invalid input
+        System.out.println("Invalid choice. Please try again.");
+      }
 
     }
     scanner.close(); //close the scanner
